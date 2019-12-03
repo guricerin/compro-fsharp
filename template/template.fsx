6032e642aa57a92e1b7991d9352547331942d649
guricerin <gurizumi@gmail.com>
2019-12-03 12:34:12 +0900

    fix template

--- template/template.fsx
+++ template/template.fsx
@@ -5,6 +5,11 @@ module Cin =
     let read f = stdin.ReadLine() |> f
     let reada f = stdin.ReadLine().Split() |> Array.map f
 
+    let readInts() =
+        read string
+        |> Seq.toArray
+        |> Array.map (fun x -> Convert.ToInt32(x.ToString()))
+
 [<EntryPoint>]
 let main _ =
     printfn "hello!"
