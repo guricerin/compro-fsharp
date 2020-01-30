7cfcdc06198d63ccc1fe1d35e27a6017580425b1
guricerin <gurizumi@gmail.com>
2020-01-30 11:02:41 +0900

    [fix] segtree bug fix

--- src/Library/data-structure/SegmentTree.fs
+++ src/Library/data-structure/SegmentTree.fs
@@ -6,14 +6,22 @@ namespace Compro.DataStructure
 /// ある集合Sに対して、二項演算(S * S -> S)が与えられて、
 /// 結合律( a * b * c = (a * b) * c = a * (b * c) )と、
 /// 単位元( a * e = e * a = a を満たすe )が存在するときのS
-/// 一点更新区間取得: 狭義のセグメント木
-type SegmentTree<'Monoid> =
+/// セグメント木の種類
+/// 一点更新区間取得: (狭義の)セグメント木
+/// 区間更新一点取得: 双対セグメント木
+/// 区間更新区間取得: 遅延伝搬セグメント木
+
+/// BEGIN CUT HERE
+
+/// 一点更新区間取得
+type SegTree<'Monoid> =
     {
       /// 実データの要素数(葉ノードの数)
       size: int
       height: int
       /// モノイドの単位元
       unity: 'Monoid
+      /// 0-indexed
       nodes: 'Monoid array
       /// 二項演算
       merge: Merge<'Monoid> }
@@ -22,32 +30,41 @@ and Merge<'a> = 'a -> 'a -> 'a
 
 [<RequireQualifiedAccess>]
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
-module SegmentTree =
+module SegTree =
 
     let internal sizeAndHeight n =
         let rec loop sAcc hAcc =
             if sAcc < n then loop (sAcc <<< 1) (hAcc + 1) else (sAcc, hAcc)
         loop 1 0
 
-    let internal parent i = (i - 1) / 2
-    let internal leftChild i = (i <<< 1) + 1
-    let internal rightChild i = (i <<< 1) + 2
-    let internal leafIdx tree k = k + tree.size - 1
+    let inline internal parent i = (i - 1) / 2
+    let inline internal leftChild i = (i <<< 1) + 1
+    let inline internal rightChild i = (i <<< 1) + 2
+    let inline internal leafIdx tree k = k + tree.size - 1
 
     let init (n: int) (f: Merge<'Monoid>) (unity: 'Monoid) =
         let size, height = sizeAndHeight n
         let nodes = Array.init (size * 2 - 1) (fun _ -> unity)
-        { SegmentTree.size = size
+        { SegTree.size = size
           height = height
           unity = unity
           nodes = nodes
           merge = f }
 
-    let build (sq: 'Monoid seq) tree: unit =
-        let sq = Seq.indexed sq
-        for (i, s) in sq do
+    let build (sq: 'Monoid seq) f unity =
+        let sq = Array.ofSeq sq
+        let len = Array.length sq
+        let tree = init len f unity
+        let nodes = tree.nodes
+        // 葉ノードに値を格納
+        for i in 0 .. len - 1 do
             let li = leafIdx tree i
-            tree.nodes.[li] <- s
+            nodes.[li] <- sq.[i]
+        // 上にマージしていく
+        for i in tree.size - 2 .. -1 .. 0 do
+            let lc, rc = leftChild i, rightChild i
+            nodes.[i] <- f nodes.[lc] nodes.[rc]
+        tree
 
     /// 一点更新
     let update k x tree: unit =
@@ -62,18 +79,23 @@ module SegmentTree =
             nodes.[p] <- tree.merge nodes.[lc] nodes.[rc]
 
     let rec internal queryCore (a: int) (b: int) (k: int) (l: int) (r: int) tree: 'Monoid =
+        // 区間外
         if r <= a || b <= l then
             tree.unity
+        // 完全被覆
         elif a <= l && r <= b then
             tree.nodes.[k]
+        // 一部だけ被覆
         else
             let lc, rc, mid = leftChild k, rightChild k, (l + r) / 2
             let lv = queryCore a b lc l mid tree
             let rv = queryCore a b rc mid r tree
             tree.merge lv rv
 
+    let query a b tree: 'Monoid = queryCore a b 0 0 tree.size tree
+
     let get k tree =
         let k = leafIdx tree k
         tree.nodes.[k]
 
-    let query a b tree: 'Monoid = queryCore a b 0 0 tree.size tree
+/// END CUT HERE
